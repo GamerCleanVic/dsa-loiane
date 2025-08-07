9fa2c7815dcc4b33c26f2f9b6d94948ced3b8ac2
GamerCleanVic <jonassd2000@gmail.com>
2025-08-06 23:26:10 -0500

    contSize

--- com/loiane/estruturadados/vetor/Vetor.java
+++ com/loiane/estruturadados/vetor/Vetor.java
@@ -2,8 +2,35 @@ package com.loiane.estruturadados.vetor;
 
 public class Vetor {
 	private String[] elementos;
+	private int tamanho;
 	
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
+	}
+	
+	/*public void adiciona(String elemento) {
+		for(int i = 0; i < elementos.length; i++) {
+			if(this.elementos[i] == null) {
+				this.elementos[i] = elemento;
+				break;
+			}
+		}
+	}*/
+	
+	/*public void adiciona(String elemento) throws Exception{
+		if(this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+		}else {
+			throw new Exception("Vetor já está cheio, não é possível adcionar mais elementos.");
+		}
+	}*/
+	
+	public boolean adiciona(String elemento){
+		if(this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+			return true;
+		}return false;
 	}
 }
